26c7f502a68baf4cd9c005340d6f138433a04fe8
Kruthika S <kruthikaramya04@gmail.com>
2026-07-27 00:07:19 +0530

    Remove rewrites and add output configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/biometrics',
-        destination: '/biometric',
-      },
-    ];
+  output: "export",
+  images: {
+    unoptimized: true,
   },
 };
 
